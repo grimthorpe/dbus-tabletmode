fb6471bf033a264fa80a3f12bd511cb226e5ee11
Lisa St.John <lisa@grimthorpe.org>
2020-03-12 11:48:35 +0100

    Get the DBus service code correct, but on the session bus because permissions are easier to deal with while debugging

--- dbus-tabletmode/dbus-tabletmode.py
+++ dbus-tabletmode/dbus-tabletmode.py
@@ -1,4 +1,25 @@
+#!/usr/bin/python
+'''
+Simple script that checks the state of the tablet mode.
+
+Returns 0 if in tablet mode, otherwise 1.
+
+Can be used with the "Configurable Button" plasmoid to indicate the tablet mode status
+and to run custom scripts (e.g. show OnBoard)
+'''
+
 import dbus
 
-system_bus=dbus.SystemBus()
+#bus=dbus.SystemnBus()
+bus=dbus.SessionBus()
+
+tabletmode=bus.get_object('org.grimthorpe.TabletMode', '/TabletMode')
+mode=tabletmode.GetTabletMode()
+
+print("The current mode is", mode)
+
+if(mode):
+    exit(0)
+else:
+    exit(1)
 
--- dbus-tabletmode/tabletmode-service.py
+++ dbus-tabletmode/tabletmode-service.py
@@ -1,24 +1,51 @@
 import dbus
+import dbus.service
 
+'''
+TabletMode class
+
+Defines 2 DBus methods:
+    SetTabletMode(boolean tabletmode)
+    boolean GetTabletMode()
+Defines 1 DBus signal:
+    TabletModeChanged(boolean tabletmode)
+'''
 class TabletMode(dbus.service.Object):
-    def __init__(self, object_path):
-        dbus.service.Object.__init__(self, dbus.SystemBus(), path)
+    def __init__(self):
         self.tabletmode=False;
+        #self.bus=dbus.SystemBus()
+        self.bus=dbus.SessionBus()
+        name=dbus.service.BusName('org.grimthorpe.TabletMode', bus=self.bus)
+        super().__init__(name, '/TabletMode')
 
+    ''' Set the TabletMode '''
     @dbus.service.method(dbus_interface='org.grimthorpe.TabletMode',
                          in_signature='b', out_signature='')
     def SetTabletMode(self, tabletmode):
-        self.tabletmode=tabletmode
-        self.TabletModeChanged(tabletmode)
+        print("SetTabletMode called with %d" % tabletmode)
+        if(tabletmode != self.tabletmode):
+            self.tabletmode=tabletmode
+            self.TabletModeChanged(tabletmode)
 
+    ''' Get the TabletMode '''
     @dbus.service.method(dbus_interface='org.grimthorpe.TabletMode',
                          in_signature='', out_signature='b')
     def GetTabletMode(self):
+        print("GetTabletMode called")
         return self.tabletmode
 
+    ''' Signal that the TabletMode has changed.
+        Only signalled when actually changed.'''
     @dbus.service.signal(dbus_interface='org.grimthorpe.TabletMode',
                          signature='b')
     def TabletModeChanged(self, tabletmode):
-        print "Tablet mode changed to %d" % tabletmode
+        print("Tablet mode changed to %d" % tabletmode)
 
+if __name__ == '__main__':
+    import dbus.mainloop.glib
+    from gi.repository import GLib
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+    loop=GLib.MainLoop()
+    serviceobject=TabletMode()
+    loop.run()
 
